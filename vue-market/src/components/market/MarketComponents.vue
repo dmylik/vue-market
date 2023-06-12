e86a909e80c192c3b17468d757dcf369c3104f49
dmylik <dzmitry.mialik@gmail.com>
2023-06-12 13:57:45 +0300

    Add Components MarketComponents.vue (Mialik)

---
+++ vue-market/src/components/market/MarketComponents.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>
+            Test
+        </h1>
+        <p>{{text}}</p>
+        <input :value="text"  @input="textAdd"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MarketComponents",
+        data(){
+            return{
+                text: "Start"
+            }
+        },
+        methods:{
+            textAdd(event){
+                this.text= event.target.value.toUpperCase()
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
